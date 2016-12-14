b2b2aafc2734237d72171478cd8d43a3ac527fc8
DESKTOP-NKG80J0\gram <found75@naver.com>
2016-12-14 11:25:34 +0900

    테스트 코드

--- KanuDiarySystem/KanuDiarySystem/main_jungmin.cpp
+++ KanuDiarySystem/KanuDiarySystem/main_jungmin.cpp
@@ -2,29 +2,68 @@
 #include <stdlib.h>
 #include "Util.h"
 
+#include "KNFile.h"
+#include "CodeList.h"
+#include "Layout.h"
+
 using namespace std;
 
 int main()
 {
+	Layout::Instance()->InitLayOut();
 
-	system("mode con:cols=120 lines=100");
+	Layout::Instance()->DrawCase1();
+	//Layout::Instance()->InitLayOut();
 
+	//화면 그리기 테스트.
+	
+	
+	
+	//KNFile knFile;
+	//knFile.Open("test.txt","r+");
+	//string str;
+	//knFile.ReadLine(str);
+	//cout << str << endl;
+	////knFile.WriteLine(string("우리나라 대한민국"));
 
-	Rect ret(0,0,100,40);
-	Rect ret1(0,0,71,3);
-	Rect ret2(70,0,30,3);
-	//Rect ret(0,0,50,50);
-	Display::DrawRect(ret);
-	Display::DrawRect(ret1);
-	Display::DrawRect(ret2);
+	//CodeListMgr mgr;
+	//mgr.LoadData();
+	//mgr.Display();
 
-	//for(int i = 0; i < 60 ; i++)
+	//vector<Code*> vec;
+
+	//mgr.GetCodeData("날씨",vec);
+	//cout << "날씨 검색 자료 표시." << endl;
+	//for(int i = 0 ; i < vec.size() ; i++)
 	//{
-	//	printf("%d\n",i);
+	//	cout << vec[i]->ToString() << endl;
+	//}
+	//vec.clear();
+	//mgr.GetCodeData("기분",vec);
+	//cout << "기분 검색 자료 표시." << endl;
+	//for(int i = 0 ; i < vec.size() ; i++)
+	//{
+	//	cout << vec[i]->ToString() << endl;
 	//}
 
-	
-	getchar();
 
-	system("pasue");
+
+
+	//Rect ret(0,0,100,40);
+	//Rect ret1(0,0,71,3);
+	//Rect ret2(70,0,30,3);
+	////Rect ret(0,0,50,50);
+	//Display::DrawRect(ret);
+	//Display::DrawRect(ret1);
+	//Display::DrawRect(ret2);
+
+	////for(int i = 0; i < 60 ; i++)
+	////{
+	////	printf("%d\n",i);
+	////}
+
+	//
+	//getchar();
+
+	system("pause");
 }
