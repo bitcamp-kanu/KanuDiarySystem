d7d3309aa80c9a5233a38c7eddb9d23ea21976d4
DESKTOP-NKG80J0\gram <found75@naver.com>
2016-12-14 10:30:49 +0900

    [mod] Code Class 를 다른 곳으로 이동.

--- KanuDiarySystem/KanuDiarySystem/CodeList.cpp
+++ KanuDiarySystem/KanuDiarySystem/CodeList.cpp
@@ -1,11 +1,75 @@
 #include "CodeList.h"
+#include "KNFile.h"
+#include "Util.h"
+#include <string>
+#include "CodeItem.h"
+using namespace std;
 
-
-Code::Code(void)
+CodeListMgr::CodeListMgr()
 {
+	m_strFilePath = "ListImte.DB";
+}
+CodeListMgr::~CodeListMgr()
+{
+	RemoveAllItem();
+}
+
+int CodeListMgr::LoadData()
+{
+	KNFile knFile;
+	int nCnt = 0;
+	if(knFile.Open(m_strFilePath,"r"))
+	{
+		while(!knFile.IsEOF())
+		{
+			string str;
+			knFile.ReadLine(str);
+			vector<string> rgStr;
+			CUtil::Tokenize(str,rgStr,",");
+			if(rgStr.size() == 3)
+			{
+				Code* p = new Code(rgStr[0],rgStr[1],rgStr[2]);
+				cout << rgStr[0] << rgStr[1] << rgStr[2];
+				//cout << p->ToString();
+				m_rgCodes.push_back(p );
+			}
+		}
+	}
+	return nCnt;
+}
+int CodeListMgr::RemoveAllItem()
+{
+	int cnt = m_rgCodes.size();
+	list<Code*>::iterator iter;
+	//for (iter = m_rgCodes.begin(); iter != m_rgCodes.end(); ++iter)
+	for (iter = m_rgCodes.begin(); iter != m_rgCodes.end();)
+	{
+		delete (*iter);
+		iter = m_rgCodes.erase(iter); //이넘이 ++을 대신 한다. 삭제 하면 다음 위치로 자동 이동 된다.
+	}
+	return cnt;
+}
+void CodeListMgr::Display()
+{
+	list<Code*>::iterator iter;
+	for (iter = m_rgCodes.begin(); iter != m_rgCodes.end(); ++iter)
+	{
+		cout << (*iter)->ToString() << endl;
+		//CLog::Instance()->WriteLog((*iter)->ToString(),__FILE__,__LINE__);
+		
+	}
 }
 
 
-Code::~Code(void)
+int CodeListMgr::GetCodeData(string mainCode, vector<Code*>& vec)
 {
+	list<Code*>::iterator iter;
+	for (iter = m_rgCodes.begin(); iter != m_rgCodes.end(); ++iter)
+	{
+		if((*iter)->GetMainCode() == mainCode)
+		{
+			vec.push_back((*iter));
+		}
+	}
+	return vec.size();
 }
--- KanuDiarySystem/KanuDiarySystem/CodeList.h
+++ KanuDiarySystem/KanuDiarySystem/CodeList.h
@@ -1,8 +1,27 @@
 #pragma once
-class Code
+#include <iostream>
+#include <string>
+#include <list>
+#include <vector>
+#include "KNNode.h"
+#include "CodeItem.h"
+#include "ICodeListSearch.h"
+using namespace std;
+
+
+//코드 정보는 txt 형태로 저장 한다.
+class CodeListMgr : public ICodeListSearch
 {
+private:
+	list<Code*> m_rgCodes;
+	string m_strFilePath;
 public:
-	Code(void);
-	virtual ~Code(void);
+	virtual int GetCodeData(string mainCode, vector<Code*>& vec);
+	CodeListMgr();
+	virtual ~CodeListMgr();
+
+	int LoadData();
+	int RemoveAllItem();
+	void Display();
 };
 
